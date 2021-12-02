993af53fdde8106abffebc569d1bbcca1a712e63
Gourab <87464911+confusedHatHacker@users.noreply.github.com>
2021-12-02 14:50:22 +0530

    Update dirEnum.py

--- dirEnum.py
+++ dirEnum.py
@@ -42,7 +42,7 @@ class dirEnum:
 
 	def banner(self):
 		border = '='*100+'\n'
-		creator = 'DirEnum created by Gourab Roy\nVersion: 1.0\nMeant for legal use only!\n'
+		creator = 'DirEnum created by Gourab Roy(@confusedHatHacker)\nVersion: 1.0\nMeant for legal use only!\n'
 		settings = f'Details:\n==> URL: http://{self.ip}\n==> Wordlist: {self.wordlist}\n==> Threads: {self.threads}\n==> Extensions: {self.ext}\n==> Mode:{self.mode}\n'
 		prologue = '\nStarting Scan:\n'
 		print('\n'+border+creator+border+settings+border+prologue)
