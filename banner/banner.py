4f0f9d55c6fd07a035ed3b3abe212c3d2b42ef86
Gourab Roy <87464911+confusedHatHacker@users.noreply.github.com>
2021-12-06 19:25:22 +0530

    Update banner.py

--- banner/banner.py
+++ banner/banner.py
@@ -12,7 +12,7 @@ class Banner:
 		pass
 
 	def banner(self):
-		banner = pyfiglet.figlet_format = ('dirCrack', font='slant')
+		banner = pyfiglet.figlet_format('dirHunter', font='slant')
 		border = '='*100+'\n'
 		creator = 'dirCrack created by Gourab Roy(@confusedHatHacker)\nVersion: 1.0\nMeant for legal use only!\n'
 		settings = f'Details:\n==> URL: http://{self.ip}\n==> Wordlist: {self.wordlist}\n==> Threads: {self.threads}\n==> Extensions: {self.ext}\n==> Mode:{self.mode}\n'
