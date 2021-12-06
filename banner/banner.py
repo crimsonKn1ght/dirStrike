db666fed0c35e1f67f787aad21872f4c05567ee0
Gourab Roy <87464911+confusedHatHacker@users.noreply.github.com>
2021-12-06 19:16:10 +0530

    Update banner.py

--- banner/banner.py
+++ banner/banner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import pyfiglet
+
 class Banner:
 	def __init__(self, ip, wordlist, threads, ext, mode):
 		self.ip = ip
@@ -10,8 +12,9 @@ class Banner:
 		pass
 
 	def banner(self):
+		banner = pyfiglet.figlet_format = ('dirCrack', font='slant')
 		border = '='*100+'\n'
-		creator = 'DirEnum created by Gourab Roy(@confusedHatHacker)\nVersion: 1.0\nMeant for legal use only!\n'
+		creator = 'dirCrack created by Gourab Roy(@confusedHatHacker)\nVersion: 1.0\nMeant for legal use only!\n'
 		settings = f'Details:\n==> URL: http://{self.ip}\n==> Wordlist: {self.wordlist}\n==> Threads: {self.threads}\n==> Extensions: {self.ext}\n==> Mode:{self.mode}\n'
 		prologue = '\nStarting Scan:\n'
-		print('\n'+border+creator+border+settings+border+prologue)
+		print('\n'+banner+'\n'+border+creator+border+settings+border+prologue)
