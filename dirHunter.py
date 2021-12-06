e3db0c995cc494c59e97b19cd0b6160980badac7
Gourab Roy <87464911+confusedHatHacker@users.noreply.github.com>
2021-12-06 19:29:28 +0530

    Update and rename dirCrack.py to dirHunter.py

---
+++ dirHunter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+
+from args.arguments import argcheck
+from banner.banner import Banner
+from tools.scanner import dirHunter
+import urllib3
+
+
+if __name__=='__main__':
+
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    
+    arguments = argcheck()
+    args = arguments.Argcheck()
+
+    if args.mode:
+        mode = args.mode
+    else:
+        mode = 'dir'
+
+    if args.threads:
+        threads = args.threads
+    else:
+        threads = 40
+
+    if args.ext:
+        ext = args.ext
+    else:
+        ext = None
+
+    show_banner = Banner(args.url, args.wordlist, mode.lower(), threads, ext)
+    show_banner.banner()
+
+    scan = dirHunter(args.url, args.wordlist, mode.lower(), threads, ext)
+    try:
+        scan.dirHunter()
+    except Exception as e:
+        print(e)
+
+	
