b188b89cd275710814ae5ee451782202950d6b88
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2021-12-11 13:10:57 +0530

    Update banner.py

--- banner/banner.py
+++ banner/banner.py
@@ -14,7 +14,7 @@ class Banner:
 	def banner(self):
 		banner = pyfiglet.figlet_format('dirHunter', font='slant')
 		border = '='*100+'\n'
-		creator = 'dirHunter created by Gourab Roy(@confusedHatHacker)\nVersion: 1.0\nMeant for legal use only!\n'
+		creator = 'dirHunter created by Gourab Roy(@crimsonKn1ght)\nVersion: 1.0\nMeant for legal use only!\n'
 		settings = f'Details:\n==> URL: http://{self.ip}\n==> Wordlist: {self.wordlist}\n==> Threads: {self.threads}\n==> Extensions: {self.ext}\n==> Mode:{self.mode}\n'
 		prologue = '\nStarting Scan:\n'
 		print('\n'+banner+'\n'+border+creator+border+settings+border+prologue)
