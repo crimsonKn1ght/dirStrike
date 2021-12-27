e06b7d0aa9313ce56b2230e6faaf1b475e9c099a
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2021-12-27 12:53:54 +0530

    Update check.py

--- tools/check.py
+++ tools/check.py
@@ -4,11 +4,11 @@ import requests
 
 class check_site:
 	def __init__(self):
-		self.headers={'User-Agent':'dirHunter/1.0'}
+		self.headers={'User-Agent':'dirstrike/1.1'}
 
 	def check_site(self, site):
 		try:
-			response = requests.get(site, headers=self.headers)
+			response = requests.get(site, headers=self.headers, allow_redirects=False)
 		except requests.ConnectionError:
 			pass
 		else:
