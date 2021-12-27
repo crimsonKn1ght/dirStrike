79f0b8d03e096235adf72542e4b1cb58dfc7a70a
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2021-12-27 13:19:16 +0530

    Update dirscan.py

--- tools/dirscan.py
+++ tools/dirscan.py
@@ -9,19 +9,20 @@ from .check import check_site
 
 
 class Scan:
-    def __init__(self, ip, ext):
+    def __init__(self, ip, ext, code):
         self.ip = ip
         self.ext = ext
         self.res = check_site()
+        self.code = code
 
     def dirscan(self, Dir):
-        site = f"http://{self.ip}/{Dir}"
+        site = f"{self.ip}/{Dir}"
         response_code = self.res.check_site(site)
-        if response_code != None and response_code != 404:
+        if response_code != None and response_code != self.code[0] and response_code != self.code[1]:
         	print("{:<50}                    {:>18}".format("[+] "+site, "[Status code:"+str(response_code)+"]"))
         	if self.ext != None:
         		for ext in self.ext.split(','):
         			site = f"http://{self.ip}/{Dir}.{ext.strip()}"
         			response_code = self.res.check_site(site)
-        			if response_code != None and response_code != 404:
+        			if response_code != None and response_code != self.code[0] and response_code != self.code[1]:
         				print("{:<50}                    {:>18}".format("[+] "+site, "[Status code:"+str(response_code)+"]"))
