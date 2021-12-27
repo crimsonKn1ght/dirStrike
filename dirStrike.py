cff468a5b654056babd827db6fa8a5986e538c24
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2021-12-27 13:25:20 +0530

    Update dirStrike.py

--- dirStrike.py
+++ dirStrike.py
@@ -8,6 +8,7 @@ import urllib3
 
 
 if __name__=='__main__':
+
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     
     arguments = argcheck()
@@ -21,17 +22,31 @@ if __name__=='__main__':
     if args.threads:
         threads = args.threads
     else:
-        threads = 40
+        threads = 50
 
     if args.ext:
         ext = args.ext
     else:
         ext = None
 
-    show_banner = Banner(args.url, args.wordlist, threads, ext, mode.lower())
+    if not args.url.startswith('http://') or not args.url.startswith('https://'):
+        if not args.url.startswith('www.'):
+            url='https://www.'+args.url
+        else:
+            url='https://'+args.url
+    if args.url.startswith('https://') or args.url.startswith('http://'):
+        site=args.url.lstrip('https://')
+        site=site.lstrip('http://')
+        if not site.startswith('www.'):
+            url='https://www.'+site
+        else:
+            url=args.url
+
+
+    show_banner = Banner(url, args.wordlist, threads, ext, mode.lower())
     show_banner.banner()
 
-    scan = dirStrike(args.url, args.wordlist, mode.lower(), threads, ext)
+    scan = dirStrike(url, args.wordlist, mode.lower(), threads, ext)
     try:
         scan.dirStrike()
     except Exception as e:
