5c325208f7b9af583425e4bb67613b7328eea70e
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2021-12-27 13:19:48 +0530

    Update fuzzer.py

--- tools/fuzzer.py
+++ tools/fuzzer.py
@@ -8,13 +8,13 @@ import collections
 from .check import check_site
 
 class Fuzz:
-	def __init__(self, ip):
-		self.ip = ip
+	def __init__(self, ip, code):
+		self.ip = ip.strip('https://www.').strip('http://www.')
 		self.res = check_site()
+		self.code = code
 
 	def fuzzer(self, sub):
-		sub_domain = f"http://{sub}.{self.ip}"
+		sub_domain = f"https://{sub}.{self.ip}"
 		response_code = self.res.check_site(sub_domain)
-		if response_code != None and response_code != 404:
+		if response_code != None and response_code != self.code[0] and response_code != self.code[1]:
 			print("{:<50}                    {:>18}".format("[+] "+sub_domain, "[Status code:"+str(response_code)+"]"))
-
