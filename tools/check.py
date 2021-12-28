e11fc39debc21769919a1ea8c0016118a3510639
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2021-12-29 01:52:17 +0530

    Update check.py

--- tools/check.py
+++ tools/check.py
@@ -8,8 +8,16 @@ class check_site:
 
 	def check_site(self, site):
 		try:
-			response = requests.get(site, headers=self.headers, allow_redirects=False)
-		except requests.ConnectionError:
-			pass
+			res1 = requests.get(site, headers=self.headers)
+			if len(res1.history)>0:
+				if str(res1.history[0].status_code).startswith('3'):
+					url=res1.url
+			if res1.history[0].status_code:
+				first_code=res1.history[0].status_code
+			else:
+				first_code=None
+
+		except Exception as e:
+			print(e)
 		else:
-			return response.status_code
+			return [url, res1.status_code, first_code]
