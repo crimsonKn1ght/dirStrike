bf06da8b6004a91d11da6725e97d6b022c2d0269
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2021-12-29 01:40:09 +0530

    Update banner.py

--- banner/banner.py
+++ banner/banner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pyfiglet
+from termcolor import cprint
 
 class Banner:
 	def __init__(self, ip, wordlist, threads, ext, mode):
@@ -13,8 +14,11 @@ class Banner:
 
 	def banner(self):
 		banner = pyfiglet.figlet_format('dirStrike', font='slant')
-		border = '='*100+'\n'
-		creator = 'dirStrike created by Gourab Roy(@crimsonKn1ght)\nVersion: 1.1\nMeant for legal use only!\n'
-		settings = f'Details:\n==> URL: {self.ip}\n==> Wordlist: {self.wordlist}\n==> Threads: {self.threads}\n==> Extensions: {self.ext}\n==> Mode:{self.mode}\n'
-		prologue = '\nStarting Scan:\n'
-		print('\n'+banner+'\n'+border+creator+border+settings+border+prologue)
+		border = '='*100
+		creator = 'dirStrike created by Gourab Roy(@crimsonKn1ght)\nVersion: 1.1\nMeant for legal use only!'
+		settings = f'Details:\n==> URL: {self.ip}\n==> Wordlist: {self.wordlist}\n==> Threads: {self.threads}\n==> Extensions: {self.ext}\n==> Mode:{self.mode}'
+		prologue = '\nStarting Scan:'
+		cprint('\n'+banner, 'red')
+		cprint(border+'\n'+creator+'\n'+border, 'green')
+		cprint(settings,'blue')
+		cprint(border+'\n'+prologue, 'green')
