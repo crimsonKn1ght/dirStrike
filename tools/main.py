a25fd979670228f963e47aeacea8b1d5c6405afe
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2022-01-02 14:54:15 +0530

    Update main.py

--- tools/main.py
+++ tools/main.py
@@ -4,13 +4,13 @@ import requests
 import concurrent.futures
 import collections
 from .check import check_site
-from .dirscan import Scan
+from .dirscan import Enum
 from .fuzzer import Fuzz
 import chardet
 
 
 class dirStrike:
-    def __init__(self, ip, wordlist, mode, threads, ext):
+    def __init__(self, ip, wordlist, mode, threads, ext, retries, login, headers, cookies):
         self.ip = ip
         self.wordlist = wordlist
         self.ext = ext
@@ -20,6 +20,10 @@ class dirStrike:
         self.mode = mode
         self.res = check_site()
         self.code = [404, None]
+        self.retries = retries
+        self.login = login
+        self.headers = headers
+        self.cookies = cookies
 
     def dirStrike(self):
         file = open(self.wordlist, 'rb')
@@ -31,12 +35,12 @@ class dirStrike:
                 if not DIR.startswith('#') and DIR != '':
                     self.q.append(DIR.strip('\n\r'))
 
-        if self.mode == 'dir':
-            scan = Scan(self.ip, self.ext)
+        if self.mode.lower() == 'dir':
+            scan = Enum(self.ip, self.ext, self.retries, self.login, self.headers, self.cookies)
             with concurrent.futures.ThreadPoolExecutor(max_workers=int(self.threads)) as executor:
             	executor.map(scan.dirScan, self.q)
 
-        elif self.mode == 'fuzz':
-            fuzz = Fuzz(self.ip)
+        elif self.mode.lower() == 'fuzz':
+            fuzz = Fuzz(self.ip, self.retries, self.login, self.headers, self.cookies)
             with concurrent.futures.ThreadPoolExecutor(max_workers=int(self.threads)) as executor:
             	executor.map(fuzz.fuzzer, self.q)
