2f45e1d53465f851a5581fec251a855747abce03
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2022-01-02 14:56:53 +0530

    Update dirStrike.py

--- dirStrike.py
+++ dirStrike.py
@@ -4,7 +4,7 @@
 from args.arguments import argcheck
 from banner.banner import Banner
 from tools.main import dirStrike
-import urllib3
+import urllib3, json
 
 
 if __name__=='__main__':
@@ -29,14 +29,43 @@ if __name__=='__main__':
     else:
         ext = None
 
-    if not args.url.startswith('http://') or not args.url.startswith('https://'):
-        url='http://'+args.url
-    
+    if args.User and args.Pass:
+    	login=[args.User, args.Pass]
+    else:
+    	login = [None, None]
+
+    if args.cookies:
+    	try:
+    		cookies=json.loads(args.cookies)
+    	except json.decoder.JSONDecodeError:
+    		cookies=json.loads(args.cookies.replace('\'','\"'))
+    else:
+    	cookies=None
+
+    if args.headers:
+    	try:
+    		headers=json.loads(args.headers)
+    	except json.decoder.JSONDecodeError:
+    		headers=json.loads(args.headers.replace('\'','\"'))
+    else:
+    	headers={'User-Agent':'dirStrike/1.1'}
+    	print('done2')
+
+    if not args.url.startswith('http://') and not args.url.startswith('https://'):
+    	url='http://'+args.url
+    else:
+    	url=args.url
+
+    if args.retries:
+    	retries = args.retries
+    else:
+    	retries = 2
 
     show_banner = Banner(url, args.wordlist, threads, ext, mode.lower())
     show_banner.banner()
 
-    scan = dirStrike(url, args.wordlist, mode.lower(), threads, ext)
+    scan = dirStrike(url, args.wordlist, mode.lower(), threads, ext, retries, login, headers, cookies)
+
     try:
         scan.dirStrike()
     except Exception as e:
