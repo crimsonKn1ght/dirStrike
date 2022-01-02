6019ae05e72336edf8947de7ebd857682b4654b2
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2022-01-02 14:53:14 +0530

    Update check.py

--- tools/check.py
+++ tools/check.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 
 import requests
+from requests.auth import HTTPBasicAuth
 
 class check_site:
 	def __init__(self):
-		self.headers={'User-Agent':'dirstrike/1.1'}
+		self.count = 0
 
-	def check_site(self, site):
+	def check_site(self, site, retries, login, headers, cookies):
 		try:
-			res1 = requests.get(site, headers=self.headers)
+			res1 = requests.get(site, headers=headers, auth=HTTPBasicAuth(login[0],login[1]), cookies=cookies)
 			if len(res1.history)>0:
 				if str(res1.history[0].status_code).startswith('3'):
 					url=res1.url
-			if res1.history[0].status_code:
-				first_code=res1.history[0].status_code
-			else:
-				first_code=None
+				else:
+					url=None
+				if res1.history[0].status_code:
+					first_code=res1.history[0].status_code
+				else:
+					first_code=None
 
 		except Exception as e:
-			print(e)
+			if self.count < retries:
+				self.count+=1
+				check_site(site)
+			else:
+				print('Connection error:', e)
+
 		else:
-			return [url, res1.status_code, first_code]
+			return [url, res1.status_code, first_code, len(res1.content)]
