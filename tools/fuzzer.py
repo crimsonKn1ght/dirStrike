3616e0d1bb90dad2405880a30b06ad2026652f9c
Gourab Roy <87464911+crimsonKn1ght@users.noreply.github.com>
2022-01-02 14:53:52 +0530

    Update fuzzer.py

--- tools/fuzzer.py
+++ tools/fuzzer.py
@@ -8,15 +8,23 @@ import collections
 from .check import check_site
 
 class Fuzz:
-	def __init__(self, ip):
+	def __init__(self, ip, retries, login, headers, cookies):
 		self.ip = ip.strip('https://www.').strip('http://www.')
 		self.res = check_site()
+		self.login = login
+		self.retries = retries
+		self.headers = headers
+		self.cookies = cookies
 
 	def fuzzer(self, sub):
 		sub_domain = f"https://{sub}.{self.ip}"
-		response = self.res.check_site(sub_domain)
+		response = self.res.check_site(sub_domain, self.retries, self.login, self.headers, self.cookies)
 		if response[1] != None and response[1] != 404:
 			if response[0]==None:
-				print("{:<100}                    {:>18}".format("[+] "+sub_domain, "[Status code:"+str(response[1])+"]"))
+				print("{:<50}{:>40}".format("[+] "+site+"    [Status code:"+str(response[1])+f"]     [Size:{response[3]}]"))
 			else:
-				print("{:<100}                    {:>18}".format("[+] "+sub_domain+" --> "+response[0], "[Status code:"+str(response[2])+"]"))
+				a="[+] " + site
+				b="[Status code:" + str(response[2])+"]  ----->  " + response[0]
+				c="    [Status code:" + str(response[1]) + f"]     [Size:{response[3]}]"
+				if len(b)>100:
+					b=b[:100]+'\n'+' '*30+f"{b[100:]:<100}"
